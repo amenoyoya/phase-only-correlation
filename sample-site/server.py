e443c571922ae77626ab45ee04f70627ac67994b
amenoyoya <mtyswe3@gmail.com>
2019-09-02 21:14:51 +0900

    add: 差分スクリーンショット

--- sample-site/server.py
+++ sample-site/server.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from typing import Tuple
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def home() -> Tuple[str, int]:
-    return render_template('home.jinja')
+    site = request.args.get('site')
+    return render_template('home.jinja', stylesheets=['/static/css/home2.css'] if site == '2' else [])
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000, host='0.0.0.0')
--- screenshot.js
+++ screenshot.js
@@ -7,9 +7,13 @@ const puppeteer = require('puppeteer');
     slowMo: 500  // 動作確認しやすいようにpuppeteerの操作を遅延させる
   });
   const page = await browser.newPage();
-
+  
+  page.setViewport({width: 1200, height: 800})
   await page.goto('http://localhost:5000') // サンプルサイト訪問
   await page.screenshot({ path: 'screenshot/sample1.png' }) // スクリーンショット撮影
+
+  await page.goto('http://localhost:5000/?site=2') // サンプルサイト2
+  await page.screenshot({ path: 'screenshot/sample2.png' })
 
   await browser.close()
 })();
